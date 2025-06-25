39b56a3fa07f7d8f7c951cbb84cec89c3e5506f0
Aliaa Osama <aliaaosama296@gmail.com>
2025-06-25 15:13:19 +0300

    Product Module - Entities

---
+++ LinkDev.Talabat.Domain/Entities/Products/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Domain.Entities.Products
+{
+    public class Product : BaseAuditableEntity<int>
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public int? CategoryId { get; set; } // FK to ProductCategory Entity
+        public virtual ProductCategory? Category { get; set; }
+
+        public int? BrandId { get; set; } // FK to ProductBrand Entity
+        public virtual ProductBrand? Brand { get; set; }
+    }
+}
---
+++ LinkDev.Talabat.Domain/Entities/Products/ProductBrand.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkDev.Talabat.Domain.Entities.Products
+{
+    public class ProductBrand : BaseAuditableEntity<int>
+    {
+        public required string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}

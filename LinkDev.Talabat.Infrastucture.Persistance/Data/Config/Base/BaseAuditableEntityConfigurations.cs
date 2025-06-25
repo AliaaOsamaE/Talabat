3bc5ab636375a0a5b1a766baea58cd9abb82355d
Aliaa Osama <aliaaosama296@gmail.com>
2025-06-25 16:34:00 +0300

    Base Entities Configurations

---
+++ LinkDev.Talabat.Infrastucture.Persistance/Data/Config/Base/BaseAuditableEntityConfigurations.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace LinkDev.Talabat.Infrastucture.Persistance.Data.Config.Base
+{
+    internal class BaseAuditableEntityConfigurations <TEntity,TKey> 
+        : BaseEntityConfigurations <TEntity,TKey>
+        where TEntity : BaseAuditableEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public override void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            base.Configure(builder);
+           
+        }
+    }
+}
--- LinkDev.Talabat.Infrastucture.Persistance/GlobalUsings.cs
+++ LinkDev.Talabat.Infrastucture.Persistance/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Configuration;
+global using LinkDev.Talabat.Domain.Common;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
